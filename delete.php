91c8592d09fe807365318eb3f77777cc2a5c201f
Polina220 <63416361+Polina220@users.noreply.github.com>
2020-12-04 16:35:55 +0500

    Update delete.php

--- delete.php
+++ delete.php
@@ -6,6 +6,9 @@
 			if(isset($_GET['id'])&&isset($_GET['query'])){
                 $id = htmlentities(mysqli_real_escape_string($link, $_GET['id']));
                 $index = htmlentities(mysqli_real_escape_string($link, $_GET['query']));
+				if($index=="stock_info"){
+                    $index="stock";
+                }
                 $query = "DELETE FROM $database.$index WHERE id='$id'";
                 $result = mysqli_query($link, $query) or die ("Ошибка в запросе");
                 header("Location: info.php?Index=$index");
